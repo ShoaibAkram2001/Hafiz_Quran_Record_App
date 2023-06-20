a28b645f975f5a0ae695df6ea5c1412af60b2497
Shoaib Akram <shoaibakram2001@gmail.com>
2023-06-20 11:22:54 +0500

    Student Record Successfully Added

--- app/src/main/java/com/example/hafiz_quran_record/AddStudent.java
+++ app/src/main/java/com/example/hafiz_quran_record/AddStudent.java
@@ -2,7 +2,7 @@ package com.example.hafiz_quran_record;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.lang.Integer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -42,12 +42,13 @@ public class AddStudent extends AppCompatActivity {
                 String name = etName.getText().toString();
                 String rollNo = etRollNo.getText().toString();
                 String Class = etClass.getText().toString();
-                String Age  = etAge.getText().toString();
+                String ag=etAge.getText().toString();
+                int Age  = Integer.parseInt(ag);
 
-                if (name.isEmpty() || rollNo.isEmpty()) {
+               /* if (name.isEmpty() || rollNo.isEmpty()) {
                     //Toast.makeText(MainActivity.this, "Please enter valid data", Toast.LENGTH_SHORT).show();
                     //return;
-                }
+                }*/
                 Student student = new Student(name, rollNo, Age,Class);
                 db.insertStudent(student);
 
